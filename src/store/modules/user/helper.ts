6c4eaa83a4093940eedea31817e4d4f4b1ff5a4a
Felix Syangli <xianglifei@outlook.com>
2023-05-03 23:40:19 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -17,7 +17,7 @@ export function defaultSetting(): UserState {
     userInfo: {
       avatar: 'https://cdn.discordapp.com/attachments/1098059333268418572/1102801350582087710/safafgagf_panda_with_Aviator_cap_1f5921ab-f226-46ff-8825-1fb459a2a56f.png',
       name: 'Copilot',
-      description: 'Let's fly',
+      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
